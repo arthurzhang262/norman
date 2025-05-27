cd3bee8a2266dadf5d466cccee263a2e7f29195d
arthurzhang262 <arthur262@outlook.com>
2025-05-27 21:00:07 +0800

    add test

--- xx.js
+++ xx.js
@@ -1,5 +1,13 @@
 function xx() {
   console.log("xx");
+  arguments[0] = "hello";
+  console.log(arguments[0]);
+  console.log(arguments[1]);
+  console.log(arguments[2]);
+  console.log(arguments[3]);
+  console.log(arguments[4]);
+  console.log(arguments[5]);
+  console.log(arguments[6]);
 }
 
 xx();
